d1d59d4197dc6196f04b835a5a079648bdd011e5
vlikaa <aliveliyev123654@gmail.com>
2023-11-03 09:43:42 +0400

    updates

--- sea battle/sea battle.cpp
+++ sea battle/sea battle.cpp
@@ -3,13 +3,21 @@
 
 using namespace std;
 
+void printGameBox(const short array[][10], const short);
+
 int main()
 {
-    
-	while (true)
-	{
-		cout << " HI";
-	}
+	const short length{ 10 };
+	short gameBox[length][length]{};
+	printGameBox(gameBox, length);
 
 }
 
+void printGameBox(const short array[][10], const short length) {
+	for (int i = 0; i < length; i++) {
+		for (int j = 0; j < length; j++) {
+			cout << array[i][j] << "   ";
+		}
+		cout << "\n\n";
+	}
+}
