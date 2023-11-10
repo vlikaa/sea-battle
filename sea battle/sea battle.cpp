06b1c5e8d6cfa599eab132ab9080a450407bb61a
vlikaa <aliveliyev123654@gmail.com>
2023-11-10 16:04:49 +0400

    updates

--- sea battle/sea battle.cpp
+++ sea battle/sea battle.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 enum ships
 {
-	singleTier = 49,
-	twoTier,
-	threeTier,
-	fourTier
+	singleDeck = 49,
+	twoDeck,
+	threeDeck,
+	fourDeck
 };
 
 enum function
@@ -43,6 +43,7 @@ enum colors
 
 enum keys
 {
+	enter = 13,
 	arrowUp = 72,
 	arrowDown = 80,
 	arrowLeft = 75,
@@ -53,7 +54,11 @@ int showMenu();
 
 void printGameBox(const short array[][10], const short);
 
-void gameInstruction();
+void shipMove(short array[][10], const short, const short, short, short);
+
+void fillField(short array[][10], const short);
+
+void makeShip(short array[][10], const short, short, short);
 
 void mainInstruction(short array[][10], const short);
 
@@ -138,7 +143,7 @@ void mainInstruction(short array[][10], const short length) {
 		switch (select - 1)
 		{
 		case start:
-			gameInstruction();
+			fillField(array, length);
 			break;
 		case load:
 			break;
@@ -150,43 +155,188 @@ void mainInstruction(short array[][10], const short length) {
 	}
 }
 
-void gameInstruction() {
-
-	const int length{ 10 };
-	short newArray[length][length]{};
-
-	short x{ 3 },
-		y{ 9 };
-	newArray[x][y] = 1;
+void shipMove(short array[][10], const short length, const short shipSize, short x, short y) {
 
 
+	bool turned = false;
 	while (true) {
+		short key;
 		system("cls");
-		printGameBox(newArray, length);
-		_getch();
+		printGameBox(array, length);
+		key = _getch();
+		if (key == enter)
+			return;
+		else if (key == 82 || key == 114) {
+			if (y + shipSize - 1 < length && x + shipSize - 1 < length) {
+				for (short i = 1; i < shipSize; i++) {
+					swap(array[x][y + i], array[x + i][y]);
+				}
+				if (array[x + 1][y] == 1)
+					turned = true;
+				else if (array[x][y + 1] == 1)
+					turned = false;
+			}
+			continue;
+		}
 		switch (_getch())
 		{
 		case arrowUp:
 			if (x > 0) {
-				swap(newArray[x][y], newArray[x - 1][y]);
+				for (short i = 0; i < shipSize; i++) {
+					if (turned) {
+						if (array[x + i][y] == 2) {
+							if (array[x + i - 1][y] == 1) {
+								array[x + i][y] = 1;
+								array[x + i - 1][y] = 2;
+							}
+							else {
+								array[x + i][y] = 1;
+								array[x + i - 1][y] = 1;
+							}
+						}
+						else if (array[x + i - 1][y] == 1) {
+							array[x + i][y] = 0;
+							array[x + i - 1][y] = 2;
+						}
+						else
+							swap(array[x + i][y], array[x + i - 1][y]);
+					}
+					else {
+						if (array[x][y + i] == 2) {
+							if (array[x - 1][y + i] == 1) {
+								array[x][y + i] = 1;
+								array[x - 1][y + i] = 2;
+							}
+							else {
+								array[x][y + i] = 1;
+								array[x - 1][y + i] = 1;
+							}
+						}
+						else if (array[x - 1][y + i] == 1) {
+							array[x][y + i] = 0;
+							array[x - 1][y + i] = 2;
+						}
+						else
+							swap(array[x][y + i], array[x - 1][y + i]);
+					}
+				}
 				x--;
 			}
 			break;
 		case arrowDown:
-			if (x < length - 1) {
-				swap(newArray[x][y], newArray[x + 1][y]);
+			if (x + shipSize - 1 < length - 1 && turned) {
+				for (short i = 0; i < shipSize; i++) {
+					if (array[x + shipSize - (i + 1)][y] == 2) {
+						if (array[x + 1 + shipSize - (i + 1)][y] == 1) {
+							array[x + shipSize - (i + 1)][y] = 1;
+							array[x + 1 + shipSize - (i + 1)][y] = 2;
+						}
+						else {
+							array[x + shipSize - (i + 2)][y] = 1;
+							array[x + shipSize - (i + 1)][y] = 1;
+						}
+					}
+					else if (array[x + 1 + shipSize - (i + 1)][y] == 1) {
+						array[x + shipSize - (i + 1)][y] = 0;
+						array[x + 1 + shipSize - (i + 1)][y] = 2;
+					}
+					else
+						swap(array[x + shipSize - (i + 1)][y], array[x + shipSize - i][y]);
+					system("cls");
+					printGameBox(array, length);
+				}
+				x++;
+			}
+			else if (x < length - 1 && !turned) {
+				for (short i = 0; i < shipSize; i++) {
+					if (array[x][y + i] == 2) {
+						if (array[x + 1][y + i] == 1) {
+							array[x][y + i] = 1;
+							array[x + 1][y + i] = 2;
+						}
+						else {
+							array[x][y + i] = 1;
+							array[x + 1][y + i] = 1;
+						}
+					}
+					else if (array[x + 1][y + i] == 1) {
+						array[x][y + i] = 0;
+						array[x + 1][y + i] = 2;
+					}
+					else
+						swap(array[x][y + i], array[x + 1][y + i]);
+				}
 				x++;
 			}
 			break;
 		case arrowLeft:
 			if (y > 0) {
-				swap(newArray[x][y], newArray[x][y - 1]);
+				for (short i = 0; i < shipSize; i++) {
+					if (turned) {
+						if (array[x + i][y] == 2) {
+							if (array[x + i][y - 1] == 1) {
+								array[x + i][y] = 1;
+								array[x + i][y - 1] = 2;
+							}
+							else {
+								array[x + i][y] = 1;
+								array[x + i][y - 1] = 1;
+							}
+						}
+						else if (array[x + i][y - 1] == 1) {
+							array[x + i][y] = 0;
+							array[x + i][y - 1] = 2;
+						}
+						else
+							swap(array[x + i][y], array[x + i][y - 1]);
+
+					}
+					else {
+						if (array[x][y + i] == 2) {
+							array[x][y + i] = 1;
+							array[x][y + i - 1] = 1;
+						}
+						else if (array[x][y + i - 1] == 1) {
+							array[x][y + i] = 0;
+							array[x][y + i - 1] = 2;
+						}
+						else
+							swap(array[x][y + i], array[x][y + i - 1]);
+					}
+				}
 				y--;
 			}
 			break;
 		case arrowRight:
-			if (y < length - 1) {
-				swap(newArray[x][y], newArray[x][y + 1]);
+			if (y + shipSize - 1 < length - 1) {
+				for (short i = 0; i < shipSize; i++) {
+					if (turned) {
+						if (array[x + i][y] == 2) {
+							array[x + i][y] = 1;
+							array[x + i][y + 1] = 1;
+						}
+						else if (array[x + i][y + 1] == 1) {
+							array[x + i][y] = 0;
+							array[x + i][y + 1] = 2;
+						}
+						else
+							swap(array[x + i][y], array[x + i][y + 1]);
+
+					}
+					else {
+						if (array[x][y + shipSize - (i + 1)] == 2) {
+							array[x][y + shipSize - (i + 1)] = 1;
+							array[x][y + shipSize - (i + 2)] = 1;
+						}
+						else if (array[x][y + shipSize - (i + 2)] == 1) {
+							array[x][y + shipSize - (i + 1)] = 0;
+							array[x][y + shipSize - (i + 2)] = 2;
+						}
+						else
+							swap(array[x][y + shipSize - (i + 1)], array[x][y + shipSize - (i + 2)]);
+					}
+
+				}
 				y++;
 			}
 			break;
@@ -196,30 +346,79 @@ void gameInstruction() {
 	}
 }
 
-void fillField() {
+void fillField(short array[][10], const short length) {
+
+	short xSpawn{ 4 },
+		ySpawn{ 4 };
+
 	short singleCount{ 4 },
 		twoCount{ 3 },
 		threeCount{ 2 },
 		fourCount{ 1 },
 		shipsCount = singleCount + twoCount + threeCount + fourCount;
+
+
 	while (shipsCount > 0)
 	{
+		printGameBox(array, length);
+		cout << "\n    Select ship:"
+			<< "\n\t1. single-deck ship"
+			<< "\n\t2. two-deck ship"
+			<< "\n\t3. three-deck ship"
+			<< "\n\t4. four-deck ship";
 		switch (_getch())
 		{
-		case singleTier:
+		case singleDeck:
 			if (singleCount > 0) {
-
+				makeShip(array, 1, xSpawn, ySpawn);
+				shipMove(array, length, 1, xSpawn, ySpawn);
 				singleCount--;
 			}
 			break;
-		case twoTier:
+		case twoDeck:
+			if (twoCount > 0) {
+				makeShip(array, 2, xSpawn, ySpawn);
+				shipMove(array, length, 2, xSpawn, ySpawn);
+				twoCount--;
+			}
 			break;
-		case threeTier:
+		case threeDeck:
+			if (threeCount > 0) {
+				makeShip(array, 3, xSpawn, ySpawn);
+				shipMove(array, length, 3, xSpawn, ySpawn);
+				threeCount--;
+			}
 			break;
-		case fourTier:
+		case fourDeck:
+			if (fourCount > 0) {
+				makeShip(array, 4, xSpawn, ySpawn);
+				shipMove(array, length, 4, xSpawn, ySpawn);
+				fourCount--;
+			}
 			break;
 		default:
 			break;
 		}
 	}
+}
+
+void makeShip(short array[][10], const short shipSize, short x, short y) {
+	for (int i = 0; i < shipSize; i++) {
+		array[x][y] = 1;
+		y++;
+	}
+}
+
+bool checkNear(short array[][10], const short shipSize, short x, short y) {
+	for (short i = 0; i < shipSize; i++) {
+		if (array[x - 1][y] == 1)
+			return true;
+		else if (array[x + 1][y] == 1)
+			return true;
+		else if (array[x][y - 1] == 1)
+			return true;
+		else if (array[x][y + 1] == 1)
+			return true;
+	}
+
 }
